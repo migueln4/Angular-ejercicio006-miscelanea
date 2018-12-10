3d9f62da81ab7bc27d5ae20cda968089196f655f
migueln4 <miguel444666@gmail.com>
2018-12-10 16:01:31 +0100

    Procesos asíncronos

--- src/app/components/clases/clases.component.ts
+++ src/app/components/clases/clases.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class ClasesComponent implements OnInit {
   
   alerta:string;
+  loading:boolean;
 
   propiedades:Object = {
     danger:true
@@ -14,9 +15,16 @@ export class ClasesComponent implements OnInit {
 
   constructor() { 
     this.alerta = "alert-primary";
+    this.loading = false;
   }
 
   ngOnInit() {
+  }
+
+  ejecutar() {
+    this.loading = true;
+    //Lo que hace aquí es una función en la que, pasados tres segundos, le cambia de nuevo el valor a la propiedad loading por FALSE.
+    setTimeout(()=> this.loading = false,3000)
   }
 
 }
